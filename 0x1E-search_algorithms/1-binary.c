358e8a3c9c4874fb24d21c933f54e1fdab4d90c4
OziN199 <nnekaozioma@gmail.com>
2023-04-14 13:28:28 +0000

    ju

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,12 +1,25 @@
 #include "search_algos.h"
-
+void print_array(int *array, size_t low, size_t high)
+{
+	char *delimiter = NULL; 
+	printf("searching in array: ");
+	for (; low <= high; low++)
+	{
+		if (delimiter != NULL)
+			printf("%s", delimiter);
+		printf("%d", array[low]);
+		delimiter = ",";
+	}
+	printf("/n");
+}
 int binary_search(int *array, size_t size, int value)
 {
 	size_t low = 0, mid, high = size - 1;
 
 	if (!array)
 		return(-1);
-
+	/*printing array at the begining*/
+	print_array(array, low, high);
 	while (low != high)
  	{
 		mid = (low + high) / 2;
@@ -17,7 +30,9 @@ int binary_search(int *array, size_t size, int value)
 			high = mid - 1;
 		else 
 			low = mid + 1;
+		print_array(array, low, high);
 	}
+	/* for edge case */
 	if (array[high] == value)
 		return (high);	
 	return (-1);
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,5 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
-
+void print_array(int *array, size_t low, size_t high);
 #endif
